d48176a5febc3a494d3cff6ad42f007001b42697
ndeet <andy.tasch@gmail.com>
2023-05-03 22:30:52 +0200

    Remove unused import.

--- src/Client/PaywallRequestClient.php
+++ src/Client/PaywallRequestClient.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace NodelessIO\Client;
 
 use NodelessIO\Response\PaywallRequestResponse;
-use NodelessIO\Response\PaywallResponse;
 
 class PaywallRequestClient extends AbstractClient
 {
